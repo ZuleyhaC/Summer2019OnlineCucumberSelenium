91f395438e0d57f4281f4e3bce47252c7f98dd5f
ZuleyhaC <zuleyhacakar@gmail.com>
2019-12-24 22:30:45 -0500

    SUmmer2019OnlineCucumberSelenium

---
+++ src/test/java/com/vytrack/step_definitions/Hook.java
@@ -0,0 +1,33 @@
+package com.vytrack.step_definitions;
+
+
+import io.cucumber.core.api.Scenario;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hook {
+
+    @Before
+    public void setup(){
+        System.out.println("##############################");
+        System.out.println("Test setup!");
+
+    }
+
+    @After
+    public void teardown(Scenario scenario){
+        //if test failed - do this
+        if(scenario.isFailed()){
+            System.out.println("Test failed!");
+
+        }else{
+            System.out.println("Cleanup!");
+            System.out.println("Test completed!");
+        }
+        System.out.println("##############################");
+        //after every test, we gonna close browser
+
+    }
+}
